ce18d443e8a05f9876b0d66e6bd26aac8a483c70
Alexander Callebaut <callebauta@hotmail.com>
2021-03-16 08:31:56 +0100

    npm fix

--- setup.bat
+++ setup.bat
@@ -5,13 +5,26 @@ if "%ProgramFiles(x86)%" == "" (
     set "MySQLServerPath=%ProgramFiles%\MySQL\MySQL Installer for Windows\"
     set "MySQLCommand=%ProgramFiles%\MySQL\MySQL Server 5.6\bin\"
     set "HomeCloudPath=%ProgramFiles%\HomeCloud\"
-    xcopy /s %cd% %ProgramFiles%
+    mkdir %ProgramFiles%\HomeCloud
 ) else (
     set "MySQLServerPath=%ProgramFiles(x86)%\MySQL\MySQL Installer for Windows\"
-    set "MySQLServerPath=%ProgramFiles(x86)%\MySQL\MySQL Server 5.6\bin\"
+    set "MySQLCommand=%ProgramFiles(x86)%\MySQL\MySQL Server 5.6\bin\"
     set "HomeCloudPath=%ProgramFiles%\HomeCloud\"
-    xcopy /s %cd% %ProgramFiles(x86)%
+    mkdir %ProgramFiles(x86)%\HomeCloud
 )
+
+for %%i in ("%~dp0.") do set "mypath=%%~fi"
+
+for /f "tokens=1-2 delims=:" %%a in ('ipconfig^|find "IPv4"') do set ip=%%b
+set ipAddress=%ip:~1%
+
+echo MYSQL_PASS=mysl >> %mypath%\backend\.env
+echo SENDGRID_API_KEY='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' >> %mypath%\backend\.env
+echo UPLOAD_FOLDER = / >> %mypath%\backend\.env
+
+echo REACT_APP_HOST_IP=%ipAddress% >> %mypath%\frontend\.env
+
+xcopy /e /v "%mypath%" "%HomeCloudPath%"
 
 echo Starting MySQL install ...
 %SystemRoot%\System32\msiexec.exe /i https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-web-community-8.0.23.0.msi /qn
@@ -29,26 +42,15 @@ echo NodeJs installed successfully.
 echo Configurating HomeCloud ...
 cd %MySQLCommand%
 mysql.exe -uroot -pmysql -e "CREATE DATABASE HomeCloud;"
-mysql.exe -uroot -pmysql -e "source %HomeCloudPath%db\init.sql;"
+mysql.exe --database=HomeCloud -uroot -pmysql -e "source %HomeCloudPath%db\init.sql;"
 
 rem todo fix source init file and below (Copie folder inside Program Files)
 
-for /f "tokens=1-2 delims=:" %%a in ('ipconfig^|find "IPv4"') do set ip=%%b
-set ipAddress=%ip:~1%
+cd %HomeCloudPath%/backend && CMD /C "%ProgramFiles%\nodejs\npm" i --loglevel error
 
-echo MYSQL_PASS=mysl >> ./backend/.env
-echo SENDGRID_API_KEY='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' >> %HomeCloudPath%/backend/.env
-echo UPLOAD_FOLDER = / >> %HomeCloudPath%/backend/.env
+cd %HomeCloudPath%/frontend && CMD /C "%ProgramFiles%\nodejs\npm" i --loglevel error
 
-echo REACT_APP_HOST_IP=%ipAddress% >> %HomeCloudPath%/frontend/.env
-
-cd %HomeCloudPath%/backend
-npm install
-
-cd %HomeCloudPath%/frontend
-npm install
-
-cd (cd %HomeCloudPath%/backend && nodemon) && (cd %HomeCloudPath%/frontend && npm run start)
+cd (cd %HomeCloudPath%/backend && nodemon) && (cd %HomeCloudPath%/frontend && CMD /C "%ProgramFiles%\nodejs\npm" run start)
 
 echo Do not close this windows!
 
