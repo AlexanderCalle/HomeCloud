d518d843175f609a8e483593efe0db8f5af178ce
Ali230 <callebauta@hotmail.com>
2021-04-21 15:30:38 +0200

    fetch data per page

--- backend/router/chat.js
+++ backend/router/chat.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const con = require('../connect');
 
 // Get a chat
-router.get('/getChat/:userId/:friendId', (req, res)=> {
+router.get('/getChat/:userId/:friendId', (req, res) => {
 
     const userId = req.params.userId;
     const friendId = req.params.friendId;
 
-    con.query('SELECT * FROM chats WHERE ? IN (userOne, userTwo) AND ? IN (userOne, userTwo)', [userId, friendId], (err, result)=> {
+    con.query('SELECT * FROM chats WHERE ? IN (userOne, userTwo) AND ? IN (userOne, userTwo)', [userId, friendId], (err, result) => {
         if (err) console.log(err);
-        
-        if(result[0] === undefined) {
+
+        if (result[0] === undefined) {
             res.status(201).send('No chat');
         } else {
             res.status(200).send(result[0])
@@ -22,12 +22,12 @@ router.get('/getChat/:userId/:friendId', (req, res)=> {
 
 });
 // make chat
-router.post('/makechat', (req, res)=> {
+router.post('/makechat', (req, res) => {
     const data = {
         userOne: req.body.userOne,
         userTwo: req.body.userTwo
     }
-    con.query('INSERT INTO chats SET ?', data, (err, result)=> {
+    con.query('INSERT INTO chats SET ?', data, (err, result) => {
         if (err) return res.status(500).send(err);
         res.status(200).send(result)
     });
@@ -40,21 +40,21 @@ router.post('/makechat', (req, res)=> {
 // }
 
 // Send message to chat
-router.post('/sendmessage', (req, res)=> {
-	const data = {
+router.post('/sendmessage', (req, res) => {
+    const data = {
         chatId: req.body.chatId,
-		fromUser: req.body.fromUser,
-		toUser: req.body.toUser,
-		message: req.body.message,
-		Status: 0,
+        fromUser: req.body.fromUser,
+        toUser: req.body.toUser,
+        message: req.body.message,
+        Status: 0,
         isImage: req.body.isImage
-	}
+    }
 
-	con.query('INSERT INTO messages SET ?', data, (err, result) => {
-		if(err) console.log(err);
+    con.query('INSERT INTO messages SET ?', data, (err, result) => {
+        if (err) console.log(err);
         res.status(200).send('result');
         //io.sockets.in(data.chatId).emit("message", data);
-	});
+    });
 });
 
 router.post('/sendimage', (req, res) => {
@@ -68,7 +68,7 @@ router.post('/sendimage', (req, res) => {
     }
 
     con.query('INSERT INTO messages SET ?', data, (err, result) => {
-        if(err) console.log(err);
+        if (err) console.log(err);
         res.status(200).send('sended!')
     })
 })
@@ -77,14 +77,14 @@ router.post('/sendimage', (req, res) => {
 router.get('/getMessages/:chatId', async (req, res) => {
     const chatId = req.params.chatId
 
-    con.query('SELECT * FROM messages WHERE chatId = ?', chatId, (err, result)=> {
-        if(err) console.log(err);
-		res.status(200).send(result);
-	});
+    con.query('SELECT * FROM messages WHERE chatId = ?', chatId, (err, result) => {
+        if (err) console.log(err);
+        res.status(200).send(result);
+    });
 
 });
 // Get latets messages from all chats
-router.get('/getlatestmessages/:userId', (req, res)=> {
+router.get('/getlatestmessages/:userId', (req, res) => {
 
     let query = `
         select c.maxID as chatId , d.* , c.chatId
@@ -99,20 +99,37 @@ router.get('/getlatestmessages/:userId', (req, res)=> {
         on c.maxID = d.message_id
     `
 
-	con.query(query , req.params.userId, (err, result) => {
-		if(err) console.log(err);
+    con.query(query, req.params.userId, (err, result) => {
+        if (err) console.log(err);
 
-		res.status(200).send(result);		
+        res.status(200).send(result);
 
-	})
+    })
 });
 // Update of user sees message
 router.post('/seenmessages/:chatId/:userId', (req, res) => {
-	con.query('UPDATE messages SET Status = 1 WHERE chatId = ? AND toUser = ?', [req.params.chatId, req.params.userId], (err, result)=> {
-        if(err) console.log(err);
-		res.status(200).send(result);
-	})
+    con.query('UPDATE messages SET Status = 1 WHERE chatId = ? AND toUser = ?', [req.params.chatId, req.params.userId], (err, result) => {
+        if (err) console.log(err);
+        res.status(200).send(result);
+    })
 });
 
+router.get('/getMessages/:chatId/page=:page', (req, res) => {
+    var numPerPage = 10;
+    var page = parseInt(req.params.page);
+    var skip = (page - 1) * numPerPage;
+    var limit = skip + ',' + numPerPage;
+    con.query('SELECT count(*) as numRows FROM messages WHERE chatId = ?', req.params.chatId, (err, rows, fields) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            var numRows = rows[0].numRows;
+            con.query('SELECT * FROM messages WHERE chatId = ? ORDER BY message_id DESC LIMIT ' + limit, req.params.chatId, (err, rows, fields) => {
+                if (err) return res.status(500).send(err) 
+                if (!err) return res.status(200).send(rows.reverse());
+            })
+        }
+    })
+});
 
 module.exports = router;
