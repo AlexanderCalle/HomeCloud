47e914d83ba5026e8789cb35071eddce74f0205b
Ali230 <callebauta@hotmail.com>
2021-05-18 21:50:16 +0200

    db fixes

--- db/init.sql
+++ db/init.sql
@@ -75,4 +75,5 @@ CREATE TABLE `shared` (
 
 
 INSERT INTO users set id = "0", firstname = "admin", lastname = "admin", email = "admin@admin.com", password_hash = "nqmljflmsqjdf";
-INSERT INTO folders set folder_id = 0, name = "chat", main_path = "/", user_id = "0";
+INSERT INTO folders set folder_id = -1, name = "chat", main_path = "/", user_id = "0";
+UPDATE folders SET folder_id = 0 WHERE folder_id = -1;
